c1de95bcfad79152c41b1cb640d939fba18c7980
Pedro Dias <pedrodiasfonseca@gmail.com>
2014-07-14 17:36:18 -0300

    Corrige erro sintaxe no controlador

--- app/scripts/controllers/navbarusermenu.js
+++ app/scripts/controllers/navbarusermenu.js
@@ -3,7 +3,7 @@
 angular.module('projetobrasilFrontApp')
   .controller('NavbarusermenuCtrl', function ($scope) {
     $scope.user = {
-      'firstName' : 'Boilerplate'
-      'avatarUrl' : 'images/avatars/users/augusto.jpg'
+      firstName : 'Boilerplate',
+      avatarUrl : 'images/avatars/users/augusto.jpg'
     };
   });
